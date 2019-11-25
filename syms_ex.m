8f8c501779349cf2faf6a4ac069aa8ff4226d1f1
dongxwdxw <57241835+dongxwdxw@users.noreply.github.com>
2019-11-25 17:21:27 +0800

    Add matlab function file
    new set up

---
+++ syms_ex.m
@@ -0,0 +1,9 @@
+
+close all
+clear;
+clc
+linewidth=2;
+fontsize=14;
+set(0,'DefaultLineLineWidth',linewidth) % set fiure line
+set(0,'DefaultTextFontsize',fontsize)
+set(0,'Defaultfigurecolor',[1 1 1]) % set fiure back white
